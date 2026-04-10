ffe1539811f2b098b6d907bf1d33d1fe98425776
wavesixnotes <randolph.quimson@concentrix.com>
2026-04-10 09:03:32 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const notepad = document.getElementById("notepad");
-
-// Load saved note
-notepad.value = localStorage.getItem("note") || "";
-
-function saveNote() {
-  localStorage.setItem("note", notepad.value);
-  alert("Note saved!");
-}
